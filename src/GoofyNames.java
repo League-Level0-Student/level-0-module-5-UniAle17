e3872a277466ba7f15a3ce25214eea40465f173b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-03 17:58:52 -0700

    GoofyName/code4life

--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -19,7 +19,10 @@ public class GoofyNames {
 
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
-
+		for (int i = 0; i < name.length(); i++) {
+			System.out.println(name.charAt(i));
+			
+		
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
@@ -29,7 +32,7 @@ public class GoofyNames {
 
 			
 			// 6. ADD this String  (containing 1 char) to the goofyName String
-
+		}
 		
 		// 7. Use pop-up to show user their Goofy name
 
