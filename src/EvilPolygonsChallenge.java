7a34a79cd062956217e2b3644f1972917d0d491b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-09 17:49:49 -0700

    GoofyNames EvilPolygonsChallenge

--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -8,20 +8,43 @@ public class EvilPolygonsChallenge {
 	
 	public static void main(String[] args) {
 		//1. Create a robot
+		Robot josh= new Robot();
 
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+		josh.setSpeed(100);
+		
+		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "Please pick a color for your robot", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Blue","Red", "Green"}, 0);
 		
 		//3. Set the pen color of the robot according to the value of the colorChoise variable
-		
-		//4. Ask the use how many polygons they want to be drawn.
-		
-		//5. Use the robot to draw the number of polygons the user requested.
+		if (colorChoice==0) {
+	josh.setPenColor(Color.BLUE);
+		}
+	if (colorChoice==1){
+		josh.setPenColor(Color.RED);
+	}
+	if (colorChoice==2) {
+		josh.setPenColor(Color.GREEN);
+	}
+	
+	
+	
+	
+	
+	
+	//4. Ask the user how many polygons they want to be drawn.
+	String polygon=	JOptionPane.showInputDialog("How many polygons would you like your robot to drawn?");
+		int polygons= Integer.parseInt(polygon);
+	
+	//5. Use the robot to draw the number of polygons the user requested.
+		josh.penDown();
+		josh.move(15);
 		
 		//6. Make it so your shapes do not overlap
 
 		//7. Challenge: add more colors to the Option Dialog.
 	}
-}
 
+
+
+
+}
--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -23,19 +23,33 @@ public class GoofyNames {
 			System.out.println(name.charAt(i));
 			
 		
-			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
+	// 4. Create a String variable to store the next character of the name
+		//    using .substring(start, end)
+		//    HINT: replace 'start' and 'end' to get String with character at i 
+				
+			String answer= name;
+			String one=answer.substring(i,i+1);
 			
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-
+			if(i%2==0) {
+				goofyName+=one.toUpperCase();
+				
+				
+			}
+			if(i%2==1) {
+				goofyName+=one.toLowerCase();			
+				
+			}
 			
 			// 6. ADD this String  (containing 1 char) to the goofyName String
 		}
 		
+		
+		
+	
 		// 7. Use pop-up to show user their Goofy name
-
+			JOptionPane.showMessageDialog(null,"your goofy name is, "+goofyName);
 	}
 }
 
